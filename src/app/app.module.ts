87d3edb526e4cedbb226debbd21997f3c5ed7b2e
Robotix703 <stephane.chatelle@orange.fr>
2022-01-07 12:38:07 +0000

    Add main and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.component';
+
+import { ErrorInterceptor } from './error-interceptor';
+import { ErrorComponent } from './error/error.component';
+
+//import { AuthInterceptor } from './auth/auth-interceptor';
+//import { AuthRoutingModule } from './auth/auth-routing.module';
+
+//Import des composants
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    HttpClientModule,
+    //AuthRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    //{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [ErrorComponent]
 })
 export class AppModule { }
