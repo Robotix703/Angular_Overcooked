ab97878a00854953ae885282aacfe00594fef0f5
Robotix703 <stephane.chatelle@orange.fr>
2022-09-29 21:26:15 +0200

    Fix recipe pagination

--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -12,53 +12,53 @@ const URL_BACKEND = environment.apiURL + "recipe/";
 
 export class RecipeService {
 
-    constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
-    addRecipe(title: string, numberOfLunch: number, image: File, category: string, duration: number, score: number = 0){
-        const recipeData = new FormData();
-        recipeData.append("title", title);
-        recipeData.append("numberOfLunch", numberOfLunch.toString());
-        recipeData.append("image", image, title);
-        recipeData.append("category", category);
-        recipeData.append("duration", duration.toString());
-        if(score != 0) recipeData.append("score", score.toString());
+  addRecipe(title: string, numberOfLunch: number, image: File, category: string, duration: number, score: number = 0) {
+    const recipeData = new FormData();
+    recipeData.append("title", title);
+    recipeData.append("numberOfLunch", numberOfLunch.toString());
+    recipeData.append("image", image, title);
+    recipeData.append("category", category);
+    recipeData.append("duration", duration.toString());
+    if (score != 0) recipeData.append("score", score.toString());
 
-        this.http.post<Recipe>(URL_BACKEND, recipeData)
-            .subscribe((responseData: Recipe) => {
-                this.router.navigate(["/recipe/list"]);
-            });
-    }
+    this.http.post<Recipe>(URL_BACKEND, recipeData)
+      .subscribe((responseData: Recipe) => {
+        this.router.navigate(["/recipe/list"]);
+      });
+  }
 
-    deleteRecipe(recipeID: string){
-        return this.http.delete(URL_BACKEND + recipeID);
-    }
+  deleteRecipe(recipeID: string) {
+    return this.http.delete(URL_BACKEND + recipeID);
+  }
 
-    getRecipe(recipeID: string){
-        return this.http.get<Recipe>(URL_BACKEND + "/byID?recipeID=" + recipeID);
-    }
+  getRecipe(recipeID: string) {
+    return this.http.get<Recipe>(URL_BACKEND + "/byID?recipeID=" + recipeID);
+  }
 
-    getFilteredRecipe(category: string, name: string, pageSize: number, currentPage: number){
-        return this.http.get<{ recipes: any, count: number }>(URL_BACKEND + `/filter?category=${category}&name=${name}&pageSize=${pageSize}&currentPage=${currentPage}`);
-    }
+  getFilteredRecipe(category: string, name: string, pageSize: number, currentPage: number) {
+    return this.http.get<{ recipes: any, count: number }>(URL_BACKEND + `/filter?category=${category}&name=${name}&pageSize=${pageSize}&currentPage=${currentPage + 1}`);
+  }
 
-    updateRecipe(recipeID: string, title: string, numberOfLunch: number, category: string, duration: number, score: number = 0){
-        this.http.put<string>(URL_BACKEND + recipeID, {
-            title: title,
-            numberOfLunch: numberOfLunch,
-            category: category,
-            duration: duration,
-            score: score
-        })
-        .subscribe((result) => {
-            this.router.navigate(["/recipe/list"]);
-        })
-    }
+  updateRecipe(recipeID: string, title: string, numberOfLunch: number, category: string, duration: number, score: number = 0) {
+    this.http.put<string>(URL_BACKEND + recipeID, {
+      title: title,
+      numberOfLunch: numberOfLunch,
+      category: category,
+      duration: duration,
+      score: score
+    })
+      .subscribe((result) => {
+        this.router.navigate(["/recipe/list"]);
+      })
+  }
 
-    getPrettyRecipe(recipeID: string = "", mealID: string = ""){
-        return this.http.get<PrettyRecipe>(URL_BACKEND + `/prettyRecipe?recipeID=${recipeID}&mealID=${mealID}`);
-    }
+  getPrettyRecipe(recipeID: string = "", mealID: string = "") {
+    return this.http.get<PrettyRecipe>(URL_BACKEND + `/prettyRecipe?recipeID=${recipeID}&mealID=${mealID}`);
+  }
 
-    getIngredientListForRecipe(recipeID: string = "", mealID: string = ""){
-        return this.http.get<{ingredient: Ingredient, quantity: number}[]>(URL_BACKEND + `/ingredientNeeded?recipeID=${recipeID}&mealID=${mealID}`);
-    }
+  getIngredientListForRecipe(recipeID: string = "", mealID: string = "") {
+    return this.http.get<{ ingredient: Ingredient, quantity: number }[]>(URL_BACKEND + `/ingredientNeeded?recipeID=${recipeID}&mealID=${mealID}`);
+  }
 }
